d6c9d3cf68848773d4bfa5a5e9c792c1e3fef9c1
Amante Reale <amantereale@gmail.com>
2017-03-08 21:32:00 +0100

    tabbing lol

--- src/index.js
+++ src/index.js
@@ -12,5 +12,5 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <Router history={browserHistory} routes={routes}/>
-    </Provider>, document.querySelector('.container'));
+    <Router history={browserHistory} routes={routes}/>
+</Provider>, document.querySelector('.container'));
